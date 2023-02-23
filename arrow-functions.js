9b9d6acac460f0a23109a1a42f7180a6f4b58bb6
stephenbeese <stephenbeese1@live.co.uk>
2023-02-23 16:22:42 +0000

    Rest parameter syntax

--- arrow-functions.js
+++ arrow-functions.js
@@ -6,43 +6,75 @@
 // Vanilla JavaScript Function
 
 // Arrow Function With Parameters
+// const addTwoNumbers = (a, b) => {
+//     return a + b;
+// }
+// console.log(addTwoNumbers(3, 5));
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers = (a,b) => a + b;
 
-// Implicit Returns
+// console.log(addTwoNumbers(6, 4));
 
-// Returning Multiple Lines
+// // Implicit Returns
+
+// // Returning Multiple Lines
 
 
+
+// /**
+//  * THE SPEAD OPERATOR
+//  */
+
+// //No spread operator
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+// arr2.push(4);
+// console.log("Second array:", arr2);
+// console.log("First array:", arr1);
+
+// // Copying an array
+// arr3 = [4, 5, 6];
+// let arr4 = [...arr3];
+// arr4.push(7);
+// console.log("Third array:", arr3);
+
+// console.log("Fourth array:", arr4);
+
+// // Copying an object
+// let obj1 = {a: 1, b: 2, c: 3};
+// let obj2 = {...obj1, d:4};
+// let obj3 = {...obj2, b: 5};
+// console.log("First object", obj1);
+// console.log("Second object", obj2);
+// console.log("Third object", obj3);
+
+// //copying only part of an array/object
+// let arr5 = [...arr1, {...obj1}, ...arr3, "X", "Y", "Z"];
+// console.log("Fifth array", arr5);
 
 /**
- * THE SPEAD OPERATOR
+ * REST PARAMETER SYNTAX
  */
 
-//No spread operator
-let arr1 = [1, 2, 3];
-let arr2 = arr1;
-arr2.push(4);
-console.log("Second array:", arr2)
-console.log("First array:", arr1)
+// Regular function call
+const sumAll = (a, b, c) => a + b + c;
 
-// Copying an array
-arr3 = [4, 5, 6];
-let arr4 = [...arr3]
-arr4.push(7)
-console.log("Third array:", arr3)
+let sum = sumAll(1, 2, 3);
+console.log("Sum:", sum);
 
-console.log("Fourth array:", arr4)
+// Extra arguments are ignored
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log("Sum2:", sum2);
 
-// Copying an object
-let obj1 = {a: 1, b: 2, c: 3};
-let obj2 = {...obj1, d:4}
-let obj3 = {...obj2, b: 5}
-console.log("First object", obj1)
-console.log("Second object", obj2)
-console.log("Third object", obj3)
+// Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum;
+}
 
-//copying only part of an array/object
-let arr5 = [...arr1, {...obj1}, ...arr3, "X", "Y", "Z"]
-console.log("Fifth array", arr5)
-
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log("Sum3:", sum3)
