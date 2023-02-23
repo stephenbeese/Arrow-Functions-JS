3748b1f2b7dae51b4a1f659e74ea9c8b6d35bfa1
stephenbeese <stephenbeese1@live.co.uk>
2023-02-23 17:10:32 +0000

    Destructuring

--- arrow-functions.js
+++ arrow-functions.js
@@ -78,3 +78,72 @@ const sumRest = (a, b, c, ...rest) => {
 
 let sum3 = sumRest(1, 2, 3, 4, 5, 6);
 console.log("Sum3:", sum3)
+
+
+/**
+ * Destructuring
+ */
+
+// NOT DESTRUCTURING
+// let ages =[30, 26, 27];
+// let john = ages[0]
+// let mary = ages[1]
+// let joe = ages[2]
+
+//Destructuring arrays
+let ages =[30, 26, 27];
+let [john, mary, joe] = ages;
+console.log(john, mary, joe)
+
+
+//Destructuring objects
+
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant",
+};
+
+let { mike, jill, alicia } = jobs;
+console.log(mike, jill, alicia);
+
+
+//Destructuring subsets
+
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, JohnSecondary] = languages;
+console.log(johnNative, JohnSecondary);
+
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
+
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "spanish",
+    fourthLanguage: "german",
+    fifthLanguage: "japanese"
+};
+
+let { firstLanguage, thirdLanguage } = languages2;
+console.log(firstLanguage, thirdLanguage);
+
+// Using rest parameter syntax
+
+
+let fruits = ["apple", "orange", "banana", "peach", "cherry"]
+let [favourite, secondFavourite, ...others] = fruits;
+console.log(favourite);
+console.log(secondFavourite);
+console.log(others);
+
+let favouriteFoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegetarian",
+    andrea: "steak",
+};
+let { brian, anna, ...rest } = favouriteFoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
